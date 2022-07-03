101a001e25d72adc513947704aa4060f88065eae
kofiarkoh <kofiarkoh0@gmail.com>
2022-07-03 09:42:09 +0000

    first commit

---
+++ src/main/java/com/softport/meenvspringboot/MeenvspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.softport.meenvspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeenvspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeenvspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softport/meenvspringboot/MeenvspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softport.meenvspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeenvspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
