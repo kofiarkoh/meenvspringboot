b4224ad85d77457aa36bf796371b85d2f0f4ac7d
lawrence <kofiarkoh0@gmail.com>
2023-04-30 16:53:17 +0000

    apply snake case naming

--- src/main/java/com/softport/meenvspringboot/user/PasswordResetDTO.java
+++ src/main/java/com/softport/meenvspringboot/user/PasswordResetDTO.java
@@ -1,8 +1,12 @@
 package com.softport.meenvspringboot.user;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.Data;
 
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PasswordResetDTO {
     private String phoneNumber;
     private String newPassword;
