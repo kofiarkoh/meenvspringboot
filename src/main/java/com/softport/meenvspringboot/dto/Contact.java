622352a4f1e3419d75d882dae2ec71820c9663de
lawrence <kofiarkoh0@gmail.com>
2023-05-04 23:06:21 +0000

    add json naming strategy

--- src/main/java/com/softport/meenvspringboot/dto/Contact.java
+++ src/main/java/com/softport/meenvspringboot/dto/Contact.java
@@ -1,12 +1,16 @@
 package com.softport.meenvspringboot.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Contact {
 
     private final String name;
-    private final String phoneNumber;
+    private final String phone;
 }
--- src/main/java/com/softport/meenvspringboot/dto/Group.java
+++ src/main/java/com/softport/meenvspringboot/dto/Group.java
@@ -3,13 +3,16 @@ package com.softport.meenvspringboot.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 //@NoArgsConstructor()
 @RequiredArgsConstructor
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Group {
 
     private final Long id;
