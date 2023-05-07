56f630b177c589b1577d9602be7b47623c353369
lawrence <kofiarkoh0@gmail.com>
2023-05-07 17:27:49 +0000

    add validation for recipient phone number

--- src/main/java/com/softport/meenvspringboot/dto/SendOneTimeMessageDTO.java
+++ src/main/java/com/softport/meenvspringboot/dto/SendOneTimeMessageDTO.java
@@ -2,6 +2,13 @@ package com.softport.meenvspringboot.dto;
 
 import java.util.List;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.softport.meenvspringboot.messages.MessageRecipient;
 
 import lombok.Data;
@@ -9,8 +16,14 @@ import lombok.Data;
 @Data
 public class SendOneTimeMessageDTO {
 
+    @Length(min = 1, max = 160)
     private String message;
     private String senderId;
-    private List<MessageRecipient> recipients;
+
+    @NotNull
+
+    @Size(min = 1, max = 5)
+
+    private List<@Valid MessageRecipient> recipients;
 
 }
