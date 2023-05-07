6cf9fe719615fc58ac486afac1a47023974ebe69
lawrence <kofiarkoh0@gmail.com>
2023-05-07 19:49:22 +0000

    change naming strategy

--- src/main/java/com/softport/meenvspringboot/dto/MessageDTO.java
+++ src/main/java/com/softport/meenvspringboot/dto/MessageDTO.java
@@ -2,11 +2,15 @@ package com.softport.meenvspringboot.dto;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class MessageDTO {
 
     public String senderId;
